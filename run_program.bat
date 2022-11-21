cb5fe7fa4db388f96dc83ce4ceb838e43308da43
Schweinhitti <89853722+Schweinhitti@users.noreply.github.com>
2022-11-21 14:43:51 +0100

    Updated Requirements

--- run_program.bat
+++ run_program.bat
@@ -1,6 +1,9 @@
 @echo off
+:start
 Title Package Installer
 echo Installer requirements
+python AutoRain.py
 python -m pip install -r requirements.txt --force-reinstall --no-cache
 python AutoRain.py
+goto start
 PAUSE
